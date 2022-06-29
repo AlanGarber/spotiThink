93d0bd8b0df2ca833d62a3773af441ad6a92ae1b
Alan Garber <83260692+AlanGarber@users.noreply.github.com>
2022-06-29 10:41:04 -0300

    conectar button con axios request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import {useState,useEffect} from 'react' 
 
 function App() {
+
+  const [data, setData] = useState();
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button>
+        Log In with Spotify
+      </button>
+      
       </header>
     </div>
   );
--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,14 +1,27 @@
 const axios = require('axios').default;
+var querystring = require('querystring');
 
 
 const axiosClient= axios.create({
-    baseURL:'http://localhost:3000',
-    headers:{
-        Authorization: 'Bearer ' + access_token
-    }
+    baseURL:'http://localhost:3000'
 })
-
-axios.get('/login')
-    .then(
-
+export const logIn=async()=>{
+    return axiosClient.get('https://accounts.spotify.com/authorize?'+
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }))
+        .then(response=>{
+            if(response.status<300){
+                console.log(response.data)
+                return response.data
+            }else{
+                console.log("La llamada salio, retorno error")
+            }
+        }
     )
+}
+    
